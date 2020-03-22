23ce33bc04510bd308b663bd6f8a9506c6b62f5e
Johnny Catsville <>
2020-03-22 16:00:09 +0300

    Bring back lost Lab0, Lab1

---
+++ Lab2/print_args/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Number of arguments: {args.Length}");
+            Console.Write($"Arguments: ");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write($"{args[i]} ");
+            }
+        }
+    }
+}
